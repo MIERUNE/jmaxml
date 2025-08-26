d4110360d877e258830c30c2549fe5eb70808e52
Taku Fukada <naninunenor@gmail.com>
2025-08-26 12:01:56 +0900

    expose ReportBody

--- jmaxml-rs/src/lib.rs
+++ jmaxml-rs/src/lib.rs
@@ -1,6 +1,6 @@
 pub mod model;
 mod parser;
 
-pub use model::{Control, IbHead, MeteBody, Report, SeisBody, VolcBody};
+pub use model::{Control, IbHead, Report, ReportBody};
 
 pub type Result<T> = std::result::Result<T, quick_xml::de::DeError>;
